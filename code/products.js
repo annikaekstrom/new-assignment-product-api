e0a7a7b9ab790d9224870d7262c0fdf4f8e901cf
AnnikaEkstrom <ekstromannika@hotmail.com>
2018-01-10 16:02:23 +0100

    Added products

--- code/products.js
+++ code/products.js
@@ -1,68 +1,4 @@
-import mongoose from "mongoose"
-import express from "express"
-import bodyParser from "body-parser"
-import cors from "cors"
-
-// Express setup, including JSON body parsing.
-const app = express()
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-
-// Tells express to add the "Access-Control-Allow-Origin" header to allow requests from anywhere.
-app.use(cors())
-
-// Connect to MongoDB, on the "products-api" database. If the db doesn't exist, mongo will create it.
-mongoose.connect("mongodb://localhost/products-api", { useMongoClient: true })
-
-// This makes mongo use ES6 promises, instead of its own implementation
-mongoose.Promise = Promise
-
-// Log when mongo connects, or encounters errors when trying to connect.
-mongoose.connection.on("error", err => console.error("Connection error:", err))
-mongoose.connection.once("open", () => console.log("Connected to mongodb"))
-
-// This is the beginning of a model for the Product object.
-const Product = mongoose.model("Product", {
-  name: String,
-  id: Number,
-  type: String,
-  size: String,
-  numberinPack: Number,
-  substance: String,
-  price: Number,
-  deliveryTime: String,
-  image: String,
-  description: String
-  // Add more attributes to your product here.
-})
-
-app.get("/", (req, res) => {
-  res.send("Products API")
-})
-
-// Endpoint to create a product. Send a POST to /products with a JSON body
-// with the keys and values you want to persist in the database.
-app.post("/products", (req, res) => {
-  const product = new Product(req.body)
-
-  product.save()
-    .then(() => { res.status(201).send("Product created") })
-    .catch(err => { res.status(400).send(err) })
-})
-
-// Add more endpoints here!
-app.get("/products", (req,res) => {
-  Product.find().then(allProducts => {
-    res.json(allProducts)
-  })
-})
-
-app.listen(8080, () => console.log("Products API listening on port 8080!"))
-
-
-
-
-const products = {
+const projects = {
 	"products": [{
 			"id": 1,
 			"name": "Alvedon",
@@ -211,9 +147,3 @@ const products = {
 
 	]
 }
-
-// products.products.forEach((data) => {
-//   const product = new Product(data)
-//
-//   product.save()
-// })
